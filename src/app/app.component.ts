0048b4242a775e6040f75d13d4cf5f618409c41d
Jose Iriarte <josemariairiarte@gmail>
2024-01-09 16:00:28 -0300

    Input and Output and Event handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import {Course} from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,21 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
+  // COURSES is defined in db-data.ts
+  // this information is now available
+  // at the template level in app.component.html
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];  
 
+  onCardClicked() {
+    console.log("App component - click event bubbled...");
+  }
+
+  // here as a parameter we capture the Course that
+  // has been emitted.
+  onCourseSelected(course:Course) {
+    console.log("App component - click event bubbled...", course);
+  }
 
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { COURSES } from '../../db-data';
+import { Course } from '../model/course';
+
+@Component({
+  // selector defines what the html tag is for this component
+  selector: 'course-card',
+  standalone: true,
+  imports: [CommonModule],
+  // points to the template file
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent implements OnInit {
+
+  // here we add the input property title
+  // to the course-card
+  @Input({
+    // if we defined required:true
+    // we´ll get a compilation error for
+    // this component if we don´t pass an input
+    required: true
+  })
+  // it can take as input a string
+  // title:string;
+  // or a whole object (from the model folder - see imports above)
+  course:Course;
+
+  @Output('courseSelected')
+  // we emit the custom event courseSelected
+  // which is defined in the parent app component
+  // we pass a parameter of an instance of Course
+  courseEmitter = new EventEmitter<Course>();
+
+  constructor() {}
+
+  ngOnInit(){}
+
+  onCourseViewed(){
+    console.log("card component - button clicked ");
+    // with this line we emit the event which will
+    // be caught at the parent level (in the app.component)
+    this.courseEmitter.emit(this.course);
+  }
+}
